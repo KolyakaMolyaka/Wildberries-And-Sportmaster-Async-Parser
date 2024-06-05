41c75dee09f76996477866b1d35d1423b2a995e4
Nikolay Markeev <zz5dy6@gmail.com>
2024-06-05 20:09:48 +0300

    исправлена ошибка RuntimeError EventLoop is closed

--- parsers/wb_parser/parser.py
+++ parsers/wb_parser/parser.py
@@ -6,7 +6,6 @@ import aiohttp
 from parsers.wb_parser.entities.wbmenu import WbMenu
 from parsers.wb_parser.entities.category_parser import CategoryParser
 from parsers.utilities import save_json_to_file
-from parsers.utilities.timer import Timer
 
 
 async def main(menu: WbMenu, category_name: str, subcategory_name: str, pages=10):
@@ -14,9 +13,11 @@ async def main(menu: WbMenu, category_name: str, subcategory_name: str, pages=10
 	cat = menu.get_category(category_name)
 	sc = cat.get_subcategory(subcategory_name)
 	async with aiohttp.ClientSession() as session:
-
 		feedbacks = await CategoryParser.parse(sc, session, pages=pages)
-		return feedbacks
+		result_filename = category_name + '_' + subcategory_name
+
+	save_json_to_file([f for f in feedbacks], result_filename)
+	return feedbacks
 
 
 if __name__ == '__main__':
@@ -26,8 +27,4 @@ if __name__ == '__main__':
 	subcategory_name = input('Введите подкатегорию для парсинга (например, "Блузки и рубашки"): ')
 	pages = int(input('Введите кол-во страниц (например, "2"): '))
 
-	result_filename = category_name + '_' + subcategory_name
-	with Timer() as _:
-		feedbacks = asyncio.run(main(menu, category_name, subcategory_name, pages))
-
-	save_json_to_file([f for f in feedbacks], result_filename)
+	feedbacks = asyncio.run(main(menu, category_name, subcategory_name, pages))
